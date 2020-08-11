41abc57b7d71126704e0a46a473740a350ddf3fd
GVU-GAME <56762234+GVU-GAME@users.noreply.github.com>
2020-08-11 18:17:20 -0400

    Updates

---
+++ precache-manifest.bd79c091fbcc4fcc8e0723d72581262b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275a0f5db0e8f1c13d973feaeb5be0d9",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "a611736d974667122c51",
+    "url": "/covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "fa63603675aa33f25437",
+    "url": "/covid-tracker/static/css/main.1b57faff.chunk.css"
+  },
+  {
+    "revision": "a611736d974667122c51",
+    "url": "/covid-tracker/static/js/2.2ff6ffa3.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-tracker/static/js/2.2ff6ffa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa63603675aa33f25437",
+    "url": "/covid-tracker/static/js/main.3fdd6f25.chunk.js"
+  },
+  {
+    "revision": "67f2510ad273685b57cb",
+    "url": "/covid-tracker/static/js/runtime-main.25272686.js"
+  }
+]);
